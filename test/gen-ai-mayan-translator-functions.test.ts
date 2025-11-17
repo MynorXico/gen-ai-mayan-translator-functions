2eb7d8f8241da96c13e51c6b6d2c391b32da510e
Mynor Xico <contact@mynorxico.me>
2025-11-16 21:13:11 -0600

    Initial commit

---
+++ test/gen-ai-mayan-translator-functions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GenAiMayanTranslatorFunctions from '../lib/gen-ai-mayan-translator-functions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gen-ai-mayan-translator-functions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GenAiMayanTranslatorFunctions.GenAiMayanTranslatorFunctionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
