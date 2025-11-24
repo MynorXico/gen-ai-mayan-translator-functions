1283a5e7d9804dfb5953af023b63d85e1e96fdff
Mynor Xico <contact@mynorxico.me>
2025-11-24 10:46:15 -0600

    Fix type issues

--- config/index.ts
+++ config/index.ts
@@ -4,14 +4,14 @@ import {Configuration} from "./types";
 export const getConfig = (): Configuration => {
     const config = {
         stackName: 'gen-ai-mayan-translator-functions',
-        apiDomainName: process.env.PARAM_API_DOMAINNAME,
-        apiSubdomainName: process.env.PARAM_API_SUBDOMAINNAME,
+        apiDomainName: process.env.PARAM_API_DOMAINNAME || '',
+        apiSubdomainName: process.env.PARAM_API_SUBDOMAINNAME || '',
         accounts: {
-            dev: process.env.PARAM_ACCOUNTS_DEV,
-            qa: process.env.PARAM_ACCOUNTS_QA,
-            prod: process.env.PARAM_ACCOUNTS_PROD
+            dev: process.env.PARAM_ACCOUNTS_DEV || '',
+            qa: process.env.PARAM_ACCOUNTS_QA || '',
+            prod: process.env.PARAM_ACCOUNTS_PROD || ''
         },
-        region: process.env.PARAM_REGION
+        region: process.env.PARAM_REGION || ''
     };
 
     // Log the configuration being used
